b685a109bb36647812359470e126aee9dd857bbc
unknown <vivekgupta@qainfotech.com>
2021-04-15 15:42:20 +0530

    poll

---
+++ src/test/java/jenkins/Testinggg.java
@@ -0,0 +1,48 @@
+package jenkins;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testinggg {
+	@Test
+	public void aisehi() {
+		System.out.println("kuch bhi");
+		
+	}
+
+	@Test(groups = "pre-tests")
+	public void init() {
+		System.out.println("init resources");
+		
+	}
+
+	@Test(dependsOnGroups = "pre-tests")
+	public void insert() {
+		System.out.println("inserting demo data");
+		
+	}
+
+	@Test(dependsOnMethods = "insert", groups = "tests")
+	public void select() {
+		System.out.println("selecting demo data");
+		System.out.println("again");
+		Assert.assertTrue(false);
+	}
+
+	@Test(dependsOnMethods = "select", groups = "tests")
+	public void update() {
+		System.out.println("updating demo data");
+	}
+
+	@Test(dependsOnMethods = { "insert", "update" }, groups = "tests")
+	public void delete() {
+		System.out.println("deleting demo data");
+	}
+
+	@Test(dependsOnGroups = "tests")
+	public void cleanup() {
+		System.out.println("closing resources");
+	}
+	
+}
